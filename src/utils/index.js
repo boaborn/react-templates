4e32bf6afada6a0d9ce89c591565322261bdcca0
Michael <michaelzhaojq@hotmail.com>
2020-04-25 23:10:23 +1200

    update eslint

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,8 +3,6 @@
  * @param {string} key
  * @return {string} value || null
  */
-export function getValueByKeyFromLocalStorage (key) {
-  const value = localStorage.getItem(key)
-  console.log('value >', value)
-  return value
+export function getValueByKeyFromLocalStorage(key) {
+  return localStorage.getItem(key)
 }
